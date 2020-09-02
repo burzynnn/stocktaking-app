1b6fa528f1ab8df8da5268ff031361b0648da502
burzynnn <kubatkh@gmail.com>
2020-09-02 18:58:27 +0200

    feat(server): create config module

---
+++ server/src/config/env.config.js
@@ -0,0 +1,22 @@
+const envConfig = {
+    server: {
+        port: process.env.SRV_PORT,
+        environment: process.env.NODE_ENV,
+    },
+    postgres: {
+        password: process.env.PG_PASSWORD,
+    },
+    redis: {
+        password: process.env.REDIS_PASSWORD,
+    },
+};
+
+Object.keys(envConfig).forEach((category) => {
+    Object.keys(envConfig[category]).forEach((secret) => {
+        if (envConfig[category][secret] === undefined) {
+            throw new Error(`${category}.${secret} has no value assigned.`);
+        }
+    });
+});
+
+export default envConfig;
