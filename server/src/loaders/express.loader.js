a3353f4be1d65121cc0ab721b3da6f38c64bfdab
burzynnn <kubatkh@gmail.com>
2020-09-13 00:08:01 +0200

    feat(server): set public folder and render engine in express loader

--- server/src/loaders/express.loader.js
+++ server/src/loaders/express.loader.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import bodyParser from "body-parser";
 import helmet from "helmet";
+import { join } from "path";
 
 import logger from "../utils/logger.util";
 import envConfig from "../config/env.config";
@@ -14,6 +15,10 @@ if (envConfig.server.environment === "production") {
 
 app.use(bodyParser.json());
 app.use(helmet());
+
+app.use(express.static(join(__dirname, "public")));
+app.set("views", join(__dirname, "public", "views"));
+app.set("view engine", "pug");
 
 const server = createServer(app).listen(envConfig.server.port, () => {
     logger.info(`HTTP server started on port ${envConfig.server.port}.`, { label: "express" });
