3849d946b3576a9a1cd350fbba332c0276454007
burzynnn <kubatkh@gmail.com>
2020-09-13 00:06:53 +0200

    feat(server): add simple middlewares to express loader

--- server/src/loaders/express.loader.js
+++ server/src/loaders/express.loader.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { createServer } from "http";
+import bodyParser from "body-parser";
+import helmet from "helmet";
 
 import logger from "../utils/logger.util";
 import envConfig from "../config/env.config";
@@ -9,6 +11,9 @@ const app = express();
 if (envConfig.server.environment === "production") {
     app.set("trust proxy", 1);
 }
+
+app.use(bodyParser.json());
+app.use(helmet());
 
 const server = createServer(app).listen(envConfig.server.port, () => {
     logger.info(`HTTP server started on port ${envConfig.server.port}.`, { label: "express" });
