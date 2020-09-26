671c7679b1baf032744ac1f4baa31bd5f1991d8f
burzynnn <kubatkh@gmail.com>
2020-09-26 15:40:11 +0200

    feat(server): create company model

---
+++ server/src/modules/company/company.model.js
@@ -0,0 +1,31 @@
+import { Model, DataTypes } from "sequelize";
+import { v4 as uuidv4 } from "uuid";
+
+import dbConnection from "../../loaders/postgres.loader";
+
+class Company extends Model {}
+
+Company.init({
+    uuid: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: () => uuidv4(),
+        allowNull: false,
+        unique: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    official_email: {
+        type: DataTypes.STRING(254),
+        allowNull: false,
+        unique: true,
+    },
+}, {
+    sequelize: dbConnection,
+    modelName: "company",
+    timestamps: true,
+});
+
+export default Company;
