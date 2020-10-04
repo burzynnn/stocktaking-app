465e98689faa83bae8f2d9346b6d80262bdc4257
burzynnn <kubatkh@gmail.com>
2020-10-04 19:21:27 +0200

    feat: create foreign key manually due to sequelize issues

--- server/src/loaders/postgres.loader.js
+++ server/src/loaders/postgres.loader.js
@@ -16,9 +16,10 @@ dbConnection
         } else {
             await dbConnection.sync({
                 // set to false in case you don't want to rebuild tables
-                alter: true,
+                force: true,
             });
         }
+        await dbConnection.query("ALTER TABLE users DROP CONSTRAINT IF EXISTS users_user_type_uuid_fkey;ALTER TABLE users ADD CONSTRAINT users_user_type_uuid_fkey FOREIGN KEY (user_type_uuid) REFERENCES user_types (uuid)");
     })
     // TODO: exit on connection issues
     .catch((err) => logger.error(`Couldn't connect to PostgreSQL. ${err}`, { label: "sequelize" }));
