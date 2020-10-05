1fe5568ba90a59c87492cac8305615a038f0592c
burzynnn <kubatkh@gmail.com>
2020-10-05 19:14:37 +0200

    feat(server): create index router and use it in app

--- server/src/loaders/express.loader.js
+++ server/src/loaders/express.loader.js
@@ -8,6 +8,8 @@ import logger from "../utils/logger.util";
 import envConfig from "../config/env.config";
 import rateLimiterMiddleware from "../middlewares/rateLimiter.middleware";
 
+import indexRouter from "../modules/index/index.router";
+
 const app = express();
 
 if (envConfig.server.environment === "production") {
@@ -23,9 +25,7 @@ app.use(express.static(join(__dirname, "public")));
 app.set("views", join(__dirname, "public", "views"));
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
-    res.send("Hi!");
-});
+app.use("/", indexRouter);
 
 const server = createServer(app).listen(envConfig.server.port, () => {
     logger.info(`HTTP server started on port ${envConfig.server.port}.`, { label: "express" });
