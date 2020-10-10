c68f441b33eb180ec8a20039b049cc38701bca5a
burzynnn <kubatkh@gmail.com>
2020-10-10 23:01:09 +0200

    feat(server): create mailing class

--- server/src/loaders/sendgrid.loader.js
+++ server/src/loaders/sendgrid.loader.js
@@ -4,4 +4,13 @@ import envConfig from "../config/env.config";
 
 sgMail.setApiKey(envConfig.sendgrid.api_key);
 
-export default sgMail;
+class Mailer {
+    constructor(sendgrid, fromAddress) {
+        this.sg = sendgrid;
+        this.from = fromAddress;
+    }
+
+    sendMail = (opts) => this.sg.send({ from: this.from, ...opts });
+}
+
+export default new Mailer(sgMail, envConfig.sendgrid.send_from);
