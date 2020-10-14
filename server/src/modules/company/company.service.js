6df4ac96d633d5e36dad82003d5ba0947610fffe
burzynnn <kubatkh@gmail.com>
2020-10-14 16:04:35 +0200

    feat(server): create company service

---
+++ server/src/modules/company/company.service.js
@@ -0,0 +1,49 @@
+import { Op } from "sequelize";
+import dayjs from "dayjs";
+
+import companyModel from "./company.model";
+
+class CompanyService {
+    constructor(cM) {
+        this.companyModel = cM;
+    }
+
+    findAll = () => this.companyModel.findAll();
+
+    create = ({
+        name,
+        email,
+        hash,
+        timestamp,
+    }) => this.companyModel.create({
+        name,
+        official_email: email,
+        activation_hash: hash,
+        activation_expiration_date: timestamp,
+    });
+
+    findOneByEmail = (email) => this.companyModel.findOne({
+        where: {
+            official_email: email,
+        },
+        attributes: ["official_email"],
+    });
+
+    findOneByActivationHash = (hash) => this.companyModel.findOne({
+        where: {
+            activation_hash: hash,
+        },
+        attributes: ["uuid", "active", "activation_hash", "activation_expiration_date"],
+    });
+
+    findAllExpired = () => this.companyModel.findAll({
+        where: {
+            activation_expiration_date: {
+                [Op.lt]: dayjs().format(),
+            },
+        },
+        attributes: ["uuid", "activation_expiration_date"],
+    });
+}
+
+export default new CompanyService(companyModel);
