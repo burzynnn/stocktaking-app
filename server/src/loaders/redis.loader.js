01ef745bc3ca041d780c6110f86b90584893694a
burzynnn <kubatkh@gmail.com>
2020-10-17 01:01:53 +0200

    feat(redis): create new redis client and adjust old code

--- server/src/loaders/redis.loader.js
+++ server/src/loaders/redis.loader.js
@@ -4,11 +4,18 @@ import envConfig from "../config/env.config";
 import logger from "../utils/logger.util";
 
 // TODO: create connect/reconnect strategy
-const redisClient = new Redis({
-    host: "redis", port: 6379, username: "stocktaking", password: envConfig.redis.password, enableOfflineQueue: false,
+const redisClients = {
+    sessions: new Redis({
+        host: "redis", port: 6379, username: "stocktaking", password: envConfig.redis.password, enableOfflineQueue: false, db: 0,
+    }),
+    rateLimiting: new Redis({
+        host: "redis", port: 6379, username: "stocktaking", password: envConfig.redis.password, enableOfflineQueue: false, db: 1,
+    }),
+};
+
+Object.entries(redisClients).forEach(([type, client]) => {
+    client.on("ready", () => logger.info("Connected to Redis.", { label: `redis-${type}` }));
+    client.on("error", (err) => logger.error(`Couldn't connect to Redis. ${err}`, { label: `redis-${type}` }));
 });
 
-redisClient.on("ready", () => logger.info("Connected to Redis.", { label: "redis" }));
-redisClient.on("error", (err) => logger.error(`Couldn't connect to Redis. ${err}`, { label: "redis" }));
-
-export default redisClient;
+export default redisClients;
