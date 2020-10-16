519c63640ca59b372dd594e334a18f295b358ea6
burzynnn <kubatkh@gmail.com>
2020-10-17 01:00:23 +0200

    feat(redis): add new acl privilige due to usage of mulitple databases

--- redis/init.sh
+++ redis/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo -e "user default off\nuser stocktaking on -@all +@connection +@scripting +SET +GET +DEL +INFO +KEYS +INCRBY +PTTL +EXPIRE ~* >$REDIS_PASSWORD" > /usr/local/etc/redis/users.acl
+echo -e "user default off\nuser stocktaking on -@all +@connection +@scripting +SET +GET +DEL +INFO +KEYS +INCRBY +PTTL +EXPIRE +SELECT ~* >$REDIS_PASSWORD" > /usr/local/etc/redis/users.acl
