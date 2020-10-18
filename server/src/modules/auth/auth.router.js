550f0268ccab187e4cf254345e89bbdea538943c
burzynnn <kubatkh@gmail.com>
2020-10-19 01:08:32 +0200

    refactor(server): group requests by routes

--- server/src/modules/auth/auth.router.js
+++ server/src/modules/auth/auth.router.js
@@ -1,22 +1,27 @@
 import { Router } from "express";
+
 import authController from "./auth.controller";
 
 const router = Router();
 
-router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
+router.route("/login")
+    .get(authController.getLogin)
+    .post(authController.postLogin);
 
-router.get("/register", authController.getRegister);
-router.post("/register", authController.postRegister);
+router.route("/register")
+    .get(authController.getRegister)
+    .post(authController.postRegister);
 
 router.get("/logout", authController.getLogout);
 
 router.get("/registration-verification", authController.getRegistrationVerification);
 
-router.get("/forgot-password", authController.getForgotPassword);
-router.post("/forgot-password", authController.postForgotPassword);
+router.route("/forgot-password")
+    .get(authController.getForgotPassword)
+    .post(authController.postForgotPassword);
 
-router.get("/reset-password", authController.getResetPassword);
-router.post("/reset-password", authController.postResetPassword);
+router.route("/reset-password")
+    .get(authController.getResetPassword)
+    .post(authController.postResetPassword);
 
 export default router;
