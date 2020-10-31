63dc29a64fad1bec5f6e058d25205f6312924bec
burzynnn <kubatkh@gmail.com>
2020-10-31 19:37:52 +0100

    feat(server): create auth module validators

---
+++ server/src/modules/auth/auth.validator.js
@@ -0,0 +1,44 @@
+import { Joi } from "celebrate";
+
+export default class AuthValidator {
+    static postLogin = {
+        body: Joi.object().options({ abortEarly: false }).keys({
+            email: Joi.string().email().trim().required(),
+            password: Joi.string().trim().required(),
+        }),
+    }
+
+    static postRegister = {
+        body: Joi.object().options({ abortEarly: false }).keys({
+            companyName: Joi.string().min(1).trim().required(),
+            companyEmail: Joi.string().email().trim().required(),
+            userName: Joi.string().min(1).trim().required(),
+            userEmail: Joi.string().email().trim().required(),
+            userPassword: Joi.string().min(8).trim().required(),
+            userRepeatPassword: Joi.any().valid(Joi.ref("userPassword")).required(),
+        }),
+    }
+
+    static getRegistrationVerification = {
+        query: Joi.object().options({ abortEarly: false }).keys({
+            hash: Joi.string().length(128).trim().required(),
+            type: Joi.string().valid("company", "user").required(),
+        }),
+    }
+
+    static postForgotPassword = {
+        body: Joi.object().options({ abortEarly: false }).keys({
+            email: Joi.string().email().trim().required(),
+        }),
+    }
+
+    static postResetPassword = {
+        query: Joi.object().options({ abortEarly: false }).keys({
+            hash: Joi.string().length(128).required(),
+        }),
+        body: Joi.object().options({ abortEarly: false }).keys({
+            newPassword: Joi.string().min(8).trim().required(),
+            repeatNewPassword: Joi.any().valid(Joi.ref("newPassword")).required(),
+        }),
+    }
+}
