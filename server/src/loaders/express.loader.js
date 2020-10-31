57cfbcc76dad5eb277a5e832c3073c6c16db7ce1
burzynnn <kubatkh@gmail.com>
2020-10-31 19:45:25 +0100

    feat(server): use error middlewares

--- server/src/loaders/express.loader.js
+++ server/src/loaders/express.loader.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 import { join } from "path";
 import csrf from "csurf";
+import cookieParser from "cookie-parser";
+import flash from "connect-flash";
 
 import logger from "../utils/logger.util";
 import envConfig from "../config/env.config";
@@ -14,6 +16,8 @@ import sessionMiddleware from "../middlewares/session.middleware";
 import indexRouter from "../modules/index/index.router";
 import authRouter from "../modules/auth/auth.router";
 
+import ErrorMiddleware from "../middlewares/error.middleware";
+
 const app = express();
 
 if (envConfig.server.environment === "production") {
@@ -21,11 +25,15 @@ if (envConfig.server.environment === "production") {
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
+app.use(helmet({
+    referrerPolicy: { policy: "same-origin" },
+}));
+app.use(cookieParser(envConfig.server.session_secret));
 
 app.use(rateLimiterMiddleware);
 app.use(sessionMiddleware);
 app.use(csrf());
+app.use(flash());
 
 app.use(express.static(join(__dirname, "public")));
 app.set("views", join(__dirname, "public", "views"));
@@ -33,6 +41,9 @@ app.set("view engine", "pug");
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
+
+app.use("*", ErrorMiddleware.notFound);
+app.use(ErrorMiddleware.exceptionHandler);
 
 const server = createServer(app).listen(envConfig.server.port, () => {
     logger.info(`HTTP server started on port ${envConfig.server.port}.`, { label: "express" });
