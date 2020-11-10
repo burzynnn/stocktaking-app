9c46e0e786296db1541732feee974715a19ca681
burzynnn <kubatkh@gmail.com>
2020-11-11 00:37:02 +0100

    test(server): create root testing file

---
+++ server/tests/test.js
@@ -0,0 +1,45 @@
+import authControllerTests from "../src/modules/auth/auth.controller.test";
+
+describe("unit tests", () => {
+    describe("auth", () => {
+        describe("controller", () => {
+            authControllerTests();
+        });
+    });
+
+    describe("category", () => {
+
+    });
+
+    describe("company", () => {
+
+    });
+
+    describe("item", () => {
+
+    });
+
+    describe("stocktake", () => {
+
+    });
+
+    describe("stocktake_item", () => {
+
+    });
+
+    describe("user", () => {
+
+    });
+
+    describe("user_type", () => {
+
+    });
+});
+
+describe("integration tests", () => {
+
+});
+
+describe("e2e tests", () => {
+
+});
