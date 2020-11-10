5729504c8505077492db0d9ff887b621d5d534ac
burzynnn <kubatkh@gmail.com>
2020-11-11 00:33:27 +0100

    chore(server): create test webpack config

--- server/webpack.test.js
+++ server/webpack.test.js
@@ -1,13 +1,19 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 
 const webpackCommonConfig = require("./webpack.common");
 
 module.exports = merge(webpackCommonConfig, {
+    entry: "./tests/test.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.test.js",
+    },
     mode: "development",
     optimization: {
         minimize: false,
     },
-    devtool: "inline-source-map",
+    devtool: "source-map",
     watchOptions: {
         poll: true,
     },
