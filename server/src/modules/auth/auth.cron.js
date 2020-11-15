6e540bdc96a763dc9f4087ad2b477074f6f10b3b
burzynnn <kubatkh@gmail.com>
2020-11-15 18:12:23 +0100

    refactor(server): adjust imports and methods parameters due to last changes

--- server/src/modules/auth/auth.cron.js
+++ server/src/modules/auth/auth.cron.js
@@ -1,14 +1,19 @@
 import { CronJob } from "cron";
 
-import companyService from "../company/company.service";
-import userService from "../user/user.service";
+import CompanyService from "../company/company.service";
+import companyModel from "../company/company.model";
+import UserService from "../user/user.service";
+import userModel from "../user/user.model";
 import logger from "../../utils/logger.util";
+
+const companyService = new CompanyService(companyModel);
+const userService = new UserService(userModel);
 
 const removeNotActivatedAccounts = new CronJob("0 0 * * * *", async () => {
     logger.info("Clearing not activated accounts.", { label: "auth_cron" });
 
     // first find all expired companies and delete it and its owner
-    const allExpiredCompanies = await companyService.findAllExpired();
+    const allExpiredCompanies = await companyService.findAllExpired(["uuid"]);
     allExpiredCompanies.forEach(async (company) => {
         const companyOwner = await userService.findOwnerByCompanyUUID(company.uuid);
         await companyOwner.destroy();
@@ -16,7 +21,7 @@ const removeNotActivatedAccounts = new CronJob("0 0 * * * *", async () => {
     });
 
     // second find all expired owners and delete it and its company
-    const allExpiredOwners = await userService.findAllExpiredOwners();
+    const allExpiredOwners = await userService.findAllExpiredOwners(["uuid"]);
     allExpiredOwners.forEach(async (owner) => {
         const companyOfExpiredOwner = await owner.getCompany();
         await companyOfExpiredOwner.destroy();
