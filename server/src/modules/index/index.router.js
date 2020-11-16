62d357296c1846d8fb922af46bd438e674b2cecb
burzynnn <kubatkh@gmail.com>
2020-11-16 20:11:06 +0100

    feat(server): create dashboard route

--- server/src/modules/index/index.router.js
+++ server/src/modules/index/index.router.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 
+import AuthMiddleware from "../auth/auth.middleware";
+
 const router = Router();
 
 router.get("/", (req, res) => res.render("index/home", { title: "Stocktaking app" }));
+
+router.get("/dashboard", AuthMiddleware.authenticate, (req, res) => res.render("index/dashboard", { title: "Dashboard" }));
 
 export default router;
