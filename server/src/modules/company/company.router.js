4ce5f81b0ab87fbae6fefbcf5bda197b9c12d802
burzynnn <kubatkh@gmail.com>
2020-11-22 18:24:14 +0100

    feat(server): use validator inside routes

--- server/src/modules/company/company.router.js
+++ server/src/modules/company/company.router.js
@@ -4,10 +4,23 @@ import CompanyController from "./company.controller";
 import CompanyService from "./company.service";
 import companyModel from "./company.model";
 import AuthMiddleware from "../auth/auth.middleware";
+import CompanyValidator from "./company.validator";
 
 const router = Router();
 const companyController = new CompanyController(new CompanyService(companyModel));
+const companyValidator = new CompanyValidator();
 
-router.get("/edit", AuthMiddleware.authenticate, AuthMiddleware.isOwner, companyController.getEditOwnCompany);
+router.route("/edit")
+    .get(
+        AuthMiddleware.authenticate,
+        AuthMiddleware.isOwner,
+        companyController.getEditOwnCompany,
+    )
+    .post(
+        companyValidator.returnValidator("postEditOwnCompany"),
+        AuthMiddleware.authenticate,
+        AuthMiddleware.isOwner,
+        companyController.postEditOwnCompany,
+    );
 
 export default router;
