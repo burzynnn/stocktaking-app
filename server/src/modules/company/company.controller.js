0f6edb60a6e75582932d40a80fb51abb2dac6115
burzynnn <kubatkh@gmail.com>
2020-12-01 14:10:15 +0100

    feat(server): create postEditOwnCompany controller

--- server/src/modules/company/company.controller.js
+++ server/src/modules/company/company.controller.js
@@ -13,9 +13,39 @@ export default class CompanyController {
 
             const { name, official_email: email } = foundCompany;
 
-            return res.render("company/edit", { title: "Edit company", csrf: req.csrfToken(), inputs: { name, email } });
+            return res.render("company/edit", {
+                title: "Edit company",
+                csrf: req.csrfToken(),
+                inputs: { name, email },
+                errors: req.flash("errors")[0],
+            });
         } catch (err) {
             return next(err);
         }
     };
+
+    postEditOwnCompany = async (req, res, next) => {
+        const { userCompanyUUID: companyUUID } = req.session;
+        const { name, email } = req.body;
+        try {
+            const foundCompany = await this.companyService.findOneByUUID(companyUUID, ["uuid", "name", "official_email"]);
+
+            let hasChanged = false;
+            if (name !== foundCompany.name) {
+                foundCompany.name = name;
+                hasChanged = true;
+            }
+            if (email !== foundCompany.official_email) {
+                foundCompany.official_email = email;
+                hasChanged = true;
+            }
+            if (hasChanged) {
+                await foundCompany.save();
+            }
+
+            return res.render("company/edit", { title: "Edit company", csrf: req.csrfToken(), inputs: { name: foundCompany.name, email: foundCompany.official_email } });
+        } catch (err) {
+            return next(err);
+        }
+    }
 }
