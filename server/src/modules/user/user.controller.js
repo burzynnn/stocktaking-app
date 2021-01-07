f45b48389d3f65c2dd5f1900a4af2a4b5ba73376
burzynnn <kubatkh@gmail.com>
2021-01-07 20:18:22 +0100

    refactor(server): adjust user controllers due to changes in user services

--- server/src/modules/user/user.controller.js
+++ server/src/modules/user/user.controller.js
@@ -1,39 +1,19 @@
-import argon2 from "argon2";
-import dayjs from "dayjs";
-
 export default class UserController {
-    constructor(userService) {
+    constructor({ userService, actionMessages }) {
         this.userService = userService;
+        this.actionMessages = actionMessages;
     }
 
     getSelfUser = async (req, res, next) => {
         const { userUUID } = req.session;
+
         try {
-            const foundUser = await this.userService.findOneByUUID(userUUID, ["name", "email", "createdAt", "company_uuid"]);
-            if (!foundUser) {
-                return res.send("No user found.");
-            }
-
-            const {
-                name,
-                email,
-                createdAt,
-                user_has_user_type: { type },
-                company: { name: companyName },
-            } = foundUser;
-
-            const accountCreationDate = dayjs(createdAt).format("HH:mm DD-MM-YYYY");
+            const foundUser = await this.userService.getUserToEdit({ userUUID });
 
             return res.render("user/me", {
-                title: name,
+                title: foundUser.name,
                 csrf: req.csrfToken(),
-                inputs: {
-                    name,
-                    email,
-                    accountCreationDate,
-                    type,
-                    companyName,
-                },
+                inputs: foundUser,
                 errors: req.flash("errors")[0],
                 messages: req.flash("messages"),
             });
@@ -45,19 +25,14 @@ export default class UserController {
     postEditUserName = async (req, res, next) => {
         const { userUUID } = req.session;
         const { name } = req.body;
+
         try {
-            const foundUser = await this.userService.findOneByUUID(userUUID, ["uuid", "name"]);
+            const editedUser = await this.userService.editUserName({ userUUID, name });
 
-            if (foundUser.name === name) {
-                req.flash("messages", [{ type: "Information", text: "We accepted your request to change your user name but it doesn't differ from actual one." }]);
-                return res.redirect("/dashboard/users/me");
-            }
-            foundUser.name = name;
-            await foundUser.save();
-            req.session.userName = name;
-            res.locals.userData.userName = name;
+            req.session.userName = editedUser.name;
+            res.locals.userData.userName = editedUser.name;
 
-            req.flash("messages", [{ type: "Success", text: "User name changed." }]);
+            req.flash("messages", this.actionMessages.convertMessages("us01"));
             return res.redirect("/dashboard/users/me");
         } catch (err) {
             return next(err);
@@ -67,17 +42,11 @@ export default class UserController {
     postEditUserEmail = async (req, res, next) => {
         const { userUUID } = req.session;
         const { email } = req.body;
+
         try {
-            const foundUser = await this.userService.findOneByUUID(userUUID, ["uuid", "email"]);
+            await this.userService.editUserEmail({ userUUID, email });
 
-            if (foundUser.email === email) {
-                req.flash("messages", [{ type: "Information", text: "We accepted your request to change your user email but it doesn't differ from actual one." }]);
-                return res.redirect("/dashboard/users/me");
-            }
-            foundUser.email = email;
-            await foundUser.save();
-
-            req.flash("messages", [{ type: "Success", text: "User email changed." }]);
+            req.flash("messages", this.actionMessages.convertMessages("us02"));
             return res.redirect("/dashboard/users/me");
         } catch (err) {
             return next(err);
@@ -88,18 +57,14 @@ export default class UserController {
         const { userUUID } = req.session;
         const { password, repeatPassword } = req.body;
         if (password !== repeatPassword) {
-            req.flash("messages", [{ type: "Error", text: "Passwords aren't the same." }]);
+            req.flash("messages", this.actionMessages.convertMessages("ae06"));
             return res.redirect("/dashboard/users/me");
         }
 
         try {
-            const foundUser = await this.userService.findOneByUUID(userUUID, ["uuid", "password"]);
+            await this.userService.editUserPassword({ userUUID, password });
 
-            const newPassword = await argon2.hash(password);
-            foundUser.password = newPassword;
-            await foundUser.save();
-
-            req.flash("messages", [{ type: "Success", text: "User password changed." }]);
+            req.flash("messages", this.actionMessages.convertMessages("us03"));
             return res.redirect("/dashboard/users/me");
         } catch (err) {
             return next(err);
