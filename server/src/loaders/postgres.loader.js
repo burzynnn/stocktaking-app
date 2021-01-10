c6122d1e1550443d2dd267f669d9b9d6947c6632
burzynnn <kubatkh@gmail.com>
2021-01-10 22:26:46 +0100

    refactor(server): adjust postgres loader due to models changes

--- server/src/loaders/postgres.loader.js
+++ server/src/loaders/postgres.loader.js
@@ -1,27 +1,74 @@
-import { Sequelize } from "sequelize";
+import Sequelize from "sequelize";
 
 import envConfig from "../config/env.config";
 import logger from "../utils/logger.util";
 
-const dbConnection = new Sequelize(`postgres://stocktaking:${envConfig.postgres.password}@postgres:5432/stocktaking`, {
+import Category from "../modules/category/category.model";
+import Company from "../modules/company/company.model";
+import CompanyVerification from "../modules/company_verification/company_verification.model";
+import Item from "../modules/item/item.model";
+import Stocktake from "../modules/stocktake/stocktake.model";
+import StocktakeItem from "../modules/stocktake_item/stocktake_item.model";
+import User from "../modules/user/user.model";
+import UserVerification from "../modules/user_verification/user_verification.model";
+
+const database = {
+    models: {},
+};
+const models = [
+    Category,
+    Company,
+    CompanyVerification,
+    Item,
+    Stocktake,
+    StocktakeItem,
+    User,
+    UserVerification,
+];
+
+const connection = new Sequelize(`postgres://stocktaking:${envConfig.postgres.password}@postgres:5432/stocktaking`, {
     logging: (msg) => logger.debug(msg, { label: "sequelize" }),
+    define: {
+        underscored: true,
+        timestamps: true,
+        freezeTableName: true,
+    },
 });
 
-dbConnection
-    .authenticate()
-    .then(async () => {
-        logger.info("Connected to PostgreSQL.", { label: "sequelize" });
-        if (envConfig.server.environment === "production") {
-            await dbConnection.sync();
-        } else {
-            await dbConnection.sync({
-                // set to false in case you don't want to rebuild tables
-                force: true,
-            });
-        }
-        await dbConnection.query("ALTER TABLE users DROP CONSTRAINT IF EXISTS users_user_type_uuid_fkey;ALTER TABLE users ADD CONSTRAINT users_user_type_uuid_fkey FOREIGN KEY (user_type_uuid) REFERENCES user_types (uuid)");
-    })
-    // TODO: exit on connection issues
-    .catch((err) => logger.error(`Couldn't connect to PostgreSQL. ${err}`, { label: "sequelize" }));
+models.forEach((model) => {
+    const createdModel = model(connection);
+    database.models[createdModel.name] = createdModel;
+});
 
-export default dbConnection;
+Object.values(database.models).forEach((model) => {
+    if (typeof model.associate === "function") {
+        model.associate(database.models);
+    }
+});
+
+const initiateDatabase = async ({ force }) => {
+    try {
+        if (envConfig.server.environment === "production") {
+            await connection.sync();
+        } else {
+            await connection.sync({ force });
+        }
+
+        logger.info("Connected to PostgreSQL.", { label: "sequelize" });
+    } catch (err) {
+        logger.error(`Couldn't connect to PostgreSQL. ${err}`, { label: "sequelize" });
+        throw err;
+    }
+};
+
+export default initiateDatabase;
+export const {
+    category,
+    company,
+    companyVerification,
+    item,
+    stocktake,
+    stocktakeItem,
+    user,
+    userVerification,
+} = database.models;
