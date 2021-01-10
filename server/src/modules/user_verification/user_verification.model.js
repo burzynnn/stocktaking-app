395043838721330784d81ec7538de8df9ceb1410
burzynnn <kubatkh@gmail.com>
2021-01-10 22:12:45 +0100

    feat(server): create user_verification model

---
+++ server/src/modules/user_verification/user_verification.model.js
@@ -0,0 +1,34 @@
+import { v4 as uuidV4 } from "uuid";
+import { DataTypes } from "sequelize";
+
+export default (databaseConnection) => {
+    const UserVerification = databaseConnection.define("userVerification", {
+        uuid: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue: () => uuidV4(),
+            allowNull: false,
+            unique: true,
+        },
+        type: {
+            type: DataTypes.ENUM("activation", "email_confirmation"),
+            allowNull: false,
+        },
+        hash: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        expiration_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    }, {
+        tableName: "user_verification",
+    });
+
+    UserVerification.associate = (models) => {
+        UserVerification.belongsTo(models.user, { foreignKey: { allowNull: false } });
+    };
+
+    return UserVerification;
+};
