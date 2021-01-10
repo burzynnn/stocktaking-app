c1b2b78b7047d4ac2939680af9bb3fd2abd9c139
burzynnn <kubatkh@gmail.com>
2021-01-10 22:11:53 +0100

    feat(server): create company_verifictaion model

---
+++ server/src/modules/company_verification/company_verification.model.js
@@ -0,0 +1,34 @@
+import { v4 as uuidV4 } from "uuid";
+import { DataTypes } from "sequelize";
+
+export default (databaseConnection) => {
+    const CompanyVerification = databaseConnection.define("companyVerification", {
+        uuid: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue: () => uuidV4(),
+            allowNull: false,
+            unique: true,
+        },
+        type: {
+            type: DataTypes.ENUM("activation"),
+            allowNull: false,
+        },
+        hash: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        expiration_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    }, {
+        tableName: "company_verification",
+    });
+
+    CompanyVerification.associate = (models) => {
+        CompanyVerification.belongsTo(models.company, { foreignKey: { allowNull: false } });
+    };
+
+    return CompanyVerification;
+};
