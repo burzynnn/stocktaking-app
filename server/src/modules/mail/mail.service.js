3f9b137e260edb2a84add665700328b4076ef8ad
burzynnn <kubatkh@gmail.com>
2021-01-31 19:38:08 +0100

    refactor(server): make use of root url variable

--- server/src/modules/mail/mail.service.js
+++ server/src/modules/mail/mail.service.js
@@ -1,6 +1,9 @@
+import envConfig from "../../config/env.config";
+
 export default class MailService {
     constructor(mailSender) {
         this.mailSender = mailSender;
+        this.url = envConfig.server.url;
     }
 
     companyRegister = ({ receiver, activationHash }) => this.mailSender.sendMail({
@@ -8,10 +11,10 @@ export default class MailService {
         subject: "Confirm your company registration.",
         text: `We are happy to see your company on stocktaking-app!
         However, we have to be sure that your registration wasn't a mistake. Please go to the link you see below.
-        http://localhost:3000/auth/registration-verification?hash=${activationHash}&type=company`,
+        ${this.url}/auth/company-activation?hash=${activationHash}`,
         html: `<h1>We are happy to see your company on stocktaking-app!</h1>
         <p>However, we have to be sure that your registration wasn't a mistake. Please click link below.<p>
-        <a href='http://localhost:3000/auth/registration-verification?hash=${activationHash}&type=company'>Confirm your registration!</a>`,
+        <a href='${this.url}/auth/company-activation?hash=${activationHash}'>Confirm your registration!</a>`,
     });
 
     userRegister = ({ receiver, activationHash }) => this.mailSender.sendMail({
@@ -19,10 +22,10 @@ export default class MailService {
         subject: "Confirm your registration.",
         text: `We are happy to see you on stocktaking-app!
         However, we have to be sure that your registration wasn't a mistake. Please go to the link you see below.
-        http://localhost:3000/auth/registration-verification?hash=${activationHash}&type=user`,
+        ${this.url}/auth/user-activation?hash=${activationHash}`,
         html: `<h1>We are happy to see you on stocktaking-app!</h1>
         <p>However, we have to be sure that your registration wasn't a mistake. Please click link below.<p>
-        <a href='http://localhost:3000/auth/registration-verification?hash=${activationHash}&type=user'>Confirm your registration!</a>`,
+        <a href='${this.url}/auth/user-activation?hash=${activationHash}'>Confirm your registration!</a>`,
     });
 
     passwordForgot = ({ receiver, passwordResetHash }) => this.mailSender.sendMail({
@@ -31,10 +34,10 @@ export default class MailService {
         text: `You have received this email because of your password reset request.
         If this was not done by you you can ignore this message.
         Otherwise click link below.
-        http://localhost:3000/auth/reset-password?hash=${passwordResetHash}`,
+        ${this.url}/auth/reset-password?hash=${passwordResetHash}`,
         html: `<h1>You have received this email because of your password reset request.</h1>
         <p>If this was not done by you you can ignore this message.
         Otherwise click link below.<p>
-        <a href='http://localhost:3000/auth/reset-password?hash=${passwordResetHash}'>Reset my password!</a>`,
+        <a href='${this.url}/auth/reset-password?hash=${passwordResetHash}'>Reset my password!</a>`,
     });
 }
